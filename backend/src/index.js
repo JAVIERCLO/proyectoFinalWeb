853bb09c03aa4a555d9cf5bd87dba82b056461a2
JAVIERCLO <cha23132@uvg.edu.gt>
2026-05-14 19:07:45 -0600

    conexión a la base de datos e implementación de docker

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import itemsRouter from './routes/items.js';
+import db from './db/db.js';
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/items', itemsRouter);
+
+// Prueba de conexión a la base de datos
+await db`SELECT 1`
+    .then(() => console.log('Conexión exitosa a postgres'))
+    .catch(err => console.error('Error de conexión:', err));
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto: ${PORT}`);
