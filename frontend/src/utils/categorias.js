2aec9d4f85bbe4176a7b074a403747279b45594e
JAVIERCLO <cha23132@uvg.edu.gt>
2026-05-20 10:13:16 -0600

    categorias

---
+++ frontend/src/utils/categorias.js
@@ -0,0 +1,32 @@
+export const CATEGORIAS = [
+    {
+    id: ciudad,
+    nombre: 'Ciudad',
+    emoji: '🏙️',
+    color: '#00A2FF'
+    },
+    {
+        id: naturaleza,
+        nombre: 'Naturaleza',
+        emoji: '⛰️',
+        color: '#4CAF50'
+    },
+    {
+        id: playa,
+        nombre: 'Playa',
+        emoji: '⛱️',
+        color: '#DDB347'
+    },
+    {
+        id: historia,
+        nombre: 'Histórico',
+        emoji: '🏛️',
+        color: '#D9D9D9'
+    },
+    {
+        id: comida,
+        nombre: 'Gastronómico',
+        emoji: '🍕',
+        color: '#D62828'
+    }
+];
